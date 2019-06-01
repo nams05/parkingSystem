82f15ce217297f3d764f4f9577e6f456b3614d16
Namrata Gupta <namrata.gupta05@gmail.com>
2019-06-01 18:05:12 +0530

    added timestamps and methods to models

--- parkingLot/models.py
+++ parkingLot/models.py
@@ -3,11 +3,27 @@ from django.db import models
 # Create your models here.
 
 
-class CarDetails(models.Model):
-    colorOptions = (('BLK', 'Black'), ('WHT', 'White'), ('BLU', 'Blue'), ('RED', 'Red'))
+class TimeStampedModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CarDetails(TimeStampedModel):
+    colorOptions = (('BLACK', 'Black'), ('WHITE', 'White'), ('BLUE', 'Blue'), ('RED', 'Red'))
     registration_number = models.CharField(max_length=13)
     color = models.CharField(max_length=5, choices=colorOptions)
 
+    def __str__(self):
+        return self.registration_number + " " + self.color
 
-class SlotDetails(models.Model):
+
+class SlotDetails(TimeStampedModel):
     carId = models.ForeignKey(CarDetails, on_delete=models.CASCADE, default=0)
+
+    def __str__(self):
+        return self.carId
+
+
